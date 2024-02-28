5e5fabdbab01b68f67849505b890a0f48090bb77
José Luis Zúñiga <jose@tangibledesign.co>
2024-02-28 14:08:49 -0500

    Ejemplo con SSR, uso de ngDestroy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
+import { ContadorComponent } from './domains/info/components/contador/contador.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent
+  },
+  {
+    path: 'contador',
+    component: ContadorComponent
   }
 ];
