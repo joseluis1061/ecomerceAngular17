67db6ef802ca3c921cb8e681974921267f29452a
jose luis zuñiga <joseluis836@hotmail.com>
2024-02-29 16:23:47 -0500

    Se agrego el precio total y lista de productos en el carrito de compra

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal, OnChanges, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product.model';
 
 @Component({
@@ -8,10 +8,23 @@ import { Product } from '../../models/product.model';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges {
   @Input() cartList: Product[] = []
 
   hiddenSideMenu: boolean = true;
+  totalPrice = signal<number>(0);
+
+
+  ngOnChanges(changes: SimpleChanges): void{
+    if(changes){
+      const products = changes['cartList'].currentValue;
+      let price = products.reduce((total: number, product: Product) => {
+        return total = total + product.price;
+      }, 0);
+      this.totalPrice.set(price);
+
+    }
+  }
 
   toogleSideMenu(): void{
     this.hiddenSideMenu = !this.hiddenSideMenu;
