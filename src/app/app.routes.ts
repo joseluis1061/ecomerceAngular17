959dee9bda09b7878d59e5be495f7d261a46d20f
José Luis Zúñiga <jose@tangibledesign.co>
2024-03-06 11:33:04 -0500

    Agregando rutas con lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,25 +13,25 @@ export const routes: Routes = [
   },
   {
     path: '',
-    component: LayoutComponent,
+    loadComponent: () => import('./domains/shared/components/layout/layout.component').then(m => m.LayoutComponent),
     children:[
       {
         path: '',
-        loadComponent: () => import('./domains/products/pages/list/list.component').then(m => ListComponent)
+        loadComponent: () => import('./domains/products/pages/list/list.component').then(m => m.ListComponent)
       },
       {
         path: 'product/:pathId',
-        loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component').then(m => ProductDetailComponent)
+        loadComponent: () => import('./domains/products/pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent)
       },
       {
         path: 'about',
-        loadComponent: () => import('./domains/info/pages/about/about.component').then(m => AboutComponent)
+        loadComponent: () => import('./domains/info/pages/about/about.component').then(m => m.AboutComponent)
 
       },
     ]
   },
   {
     path: '**',
-    component: NotFoundComponent
+    loadComponent: () => import('./domains/info/pages/not-found/not-found.component').then(m => m.NotFoundComponent)
   }
 ];
